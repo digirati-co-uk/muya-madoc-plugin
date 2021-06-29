c2ac8e67b5b8ecd9c3c7381891d23ccbeae9ebf8
Stephen Fraser <stephen.fraser@digirati.com>
2021-06-29 14:35:30 +0100

    Initial commit (bootstrap plugin)

---
+++ src/plugin.ts
@@ -0,0 +1,13 @@
+import '@madoc.io/types';
+
+export function hookRoutes() {
+  return [];
+}
+
+export function hookComponents() {
+  return {};
+}
+
+export function hookBlocks() {
+  return {};
+}
